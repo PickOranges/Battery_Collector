5fd32f99f1959040e729038950f9a42c2de46b3b
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-01-19 22:40:09 +0100

    Add Snow accumulation effects on the floor.

--- Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30133
+// Compiler: 14.29.30139
 #include "C:/Users/cheng/source/repos/ue4/Battery_Collector/Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "C:/Users/cheng/source/repos/ue4/Battery_Collector/Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
